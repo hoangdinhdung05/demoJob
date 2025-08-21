e27c40ee5b22bbcbe2c4c515096634ec5bc822ce
Hoàng Đình Dũng <63916946+hoangdinhdung05@users.noreply.github.com>
2025-08-21 09:13:02 +0700

    feat: refactor layer DTO Job and layer Mapper(Job, Skill and Company)

---
+++ src/main/java/com/demoJob/demo/mapper/JobMapper.java
@@ -0,0 +1,48 @@
+package com.demoJob.demo.mapper;
+
+import com.demoJob.demo.dto.request.Job.JobRequest;
+import com.demoJob.demo.dto.response.Admin.Job.JobResponse;
+import com.demoJob.demo.entity.Company;
+import com.demoJob.demo.entity.Job;
+import com.demoJob.demo.entity.Skill;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JobMapper {
+
+    public static JobResponse toResponse(Job job) {
+        return JobResponse.builder()
+                .id(job.getId())
+                .name(job.getName())
+                .location(job.getLocation())
+                .salary(job.getSalary())
+                .quantity(job.getQuantity())
+                .level(job.getLevel())
+                .description(job.getDescription())
+                .startDate(job.getStartDate())
+                .endDate(job.getEndDate())
+                .company(CompanyMapper.toResponseJob(job.getCompany()))
+                .skills(job.getSkills().stream()
+                        .map(SkillMapper::toResponse)
+                        .collect(Collectors.toSet()))
+                .status(job.getStatus())
+                .build();
+    }
+
+    public static Job buildJob(JobRequest request, Company company, List<Skill> skillList) {
+        return Job.builder()
+                .name(request.getName())
+                .location(request.getLocation())
+                .salary(request.getSalary())
+                .quantity(request.getQuantity())
+                .level(request.getLevel())
+                .description(request.getDescription())
+                .startDate(request.getStartDate())
+                .endDate(request.getEndDate())
+                .status(request.getStatus())
+                .company(company)
+                .skills(skillList)
+                .build();
+    }
+
+}
--- src/main/java/com/demoJob/demo/service/JobService.java
+++ src/main/java/com/demoJob/demo/service/JobService.java
@@ -1,31 +1,49 @@
 package com.demoJob.demo.service;
 
-import com.demoJob.demo.dto.request.Admin.Job.JobRequest;
+import com.demoJob.demo.dto.request.Job.JobRequest;
+import com.demoJob.demo.dto.request.Job.JobUpdateRequest;
 import com.demoJob.demo.dto.response.Admin.Job.JobResponse;
 import com.demoJob.demo.dto.response.system.PageResponse;
 import com.demoJob.demo.util.enums.JobStatus;
-import java.util.List;
 
 public interface JobService {
 
+    /**
+     * Admin và người tạo Job có thể tạo Job
+     * Admin tạo job => ACTIVE luôn
+     * User tạo Job => sendmail => Admin check => ACTIVE || REJECT
+     */
     JobResponse createJob(JobRequest request);
 
-    JobResponse updateJob(long jobId, JobRequest request);
+    /**
+     * Admin và người tạo Job có thể update Job
+     */
+    JobResponse updateJob(JobUpdateRequest request);
 
+    /**
+     * Dùng chung cho Admin và nguời tạo Job
+     * Soft delete
+     */
     void deleteJob(long jobId);
 
+    /**
+     * Admin xem xét đổi status của Job khi người tạo job sendmail đến
+     * Có thể thay đổi mọi trạng thái
+     */
     JobResponse changJobStatus(long jobId, JobStatus jobStatus);
 
-    JobResponse getById(Long id);
+    /**
+     * Dùng chung logic cho cả User, Admin và người có Owner trong CTY
+     * Nhưng User chỉ xem được những Job đã ACTIVE
+     * Còn Admin và Owner(người tạo Job thì xem job)
+     */
+    JobResponse getJobById(Long id);
 
-    List<JobResponse> getByCompanyId(Long companyId);
-
-    List<JobResponse> getBySkillId(Long skillId);
-
-    List<JobResponse> searchByName(String keyword);
-
-    List<JobResponse> getAlls();
-
+    /**
+     * Dùng chung logic cho cả User, Admin và người có Owner trong CTY
+     * Nhưng User chỉ xem được những Job đã ACTIVE
+     * Còn Admin và Owner(người tạo Job thì xem all job)
+     */
     PageResponse<?> getAllPage(int page, int size);
 
 }
