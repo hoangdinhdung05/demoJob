86bb48420e29bfc3cabc867412d64500d667a361
Hoàng Đình Dũng <63916946+hoangdinhdung05@users.noreply.github.com>
2025-08-26 16:12:23 +0700

    feat: code logic user apply job (Create Resume and SendMail to HrOROwner)

--- src/main/java/com/demoJob/demo/dto/request/Resume/ResumeRequest.java
+++ src/main/java/com/demoJob/demo/dto/request/Resume/ResumeRequest.java
@@ -1,11 +1,10 @@
-package com.demoJob.demo.dto.request.Admin.Resume;
+package com.demoJob.demo.dto.request.Resume;
 
 import lombok.Getter;
 
 @Getter
 public class ResumeRequest {
+    private Long jobId;
     private String email;
     private String url;
-    private long userId;
-    private long jobId;
 }
