0fd9e556f568327c9675a211d2c19538e8a027f3
Hoàng Đình Dũng <63916946+hoangdinhdung05@users.noreply.github.com>
2025-09-03 21:17:50 +0700

    feat: config apiDocs

--- src/main/java/com/demoJob/demo/config/SecurityConfig.java
+++ src/main/java/com/demoJob/demo/config/SecurityConfig.java
@@ -39,7 +39,12 @@ public class SecurityConfig {
             "/api/auth/**",
             "/favicon.ico",
             "/ott/**",
-            "/api/otp/**"
+            "/api/otp/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-resources/**/**",
+            "/webjars/**"
     };
 
     @Bean
